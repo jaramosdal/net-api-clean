78eba421c55bcae0174e5d6db59aec3da7435864
Javier Ramos Nodal <javiramos@soliss.com>
2026-03-01 11:46:07 +0100

    Registro automático de handlers CQRS con Scrutor
    Se añade Scrutor para escaneo y registro automático de handlers de comandos, queries y eventos de dominio. Se crean interfaces base para ICommand, IQuery y sus respectivos handlers, estandarizando respuestas con Result. Mejora la arquitectura y facilita la implementación de CQRS.

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Application.Behaviors;
+﻿using Application.Abstractions.Events;
+using Application.Abstractions.Messaging;
+using Application.Behaviors;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Soliss.NuGetRepo.Mediator;
@@ -9,7 +11,22 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediator(typeof(DependencyInjection).Assembly);
+        services.Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        services.Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
 
         // Registrar behavior de validación para comandos y queries (open-generic)
